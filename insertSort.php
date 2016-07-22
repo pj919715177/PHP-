389ebb5db257055f4f6716acfcd43c6abd00be98
unknown <pengjun@ycf.com>
2016-07-22 17:19:45 +0800

    pj

--- insertSort.php
+++ insertSort.php
@@ -1,21 +1,40 @@
 <?php
-function insertSort($arr, $end, $star = 0)
+function insertionSort($arr, $end, $start = 0)
 {
     $index = 1;
     while ($end >= $index) {
-        $position = dichotomySearch($arr, $end - 1, $star, $arr[$end]); 
-        $temp = $arr[$position];
-        $arr[$position] = $arr[$index];
-        for ($i = $position + 1; $i < $index; $i ++) {
-            $arr[$i] = $arr[$i+1];
+        $position = dichotomySearch($arr, $index - 1, $start, $arr[$index]);
+        $temp = $arr[$index];
+        for ($i = $index; $i > $position; $i--) {
+            $arr[$i] = $arr[$i - 1];
         }
-        $arr[$index] = $temp;
-        $index ++;
+        $arr[$position] = $temp;
+        $index++;
     }
     return $arr;
 }
-
-function dichotomySearch($arr, $end, $star, $basic)
+//二分查找
+function dichotomySearch($arr, $end, $start, $basic)
 {
-
+    if ($end - $start > 1) {
+        $middle = floor(($end + $start) / 2);
+        if ($arr[$middle] >= $basic) {
+            return dichotomySearch($arr, $middle, $start, $basic);
+        } else {
+            return dichotomySearch($arr, $end, $middle + 1, $basic);
+        }
+    } elseif ($basic < $arr[$start]) {
+        return $start;
+    } elseif ($basic < $arr[$end]) {
+        return $end;
+    } else {
+        return $end + 1;
+    }
 }
+
+$arr = [34, 6, 6, 75, 75, 254, 26, 885, 5463, -34, 6456, -435, -4, 525, 5643252];
+echo 'before:<br/>';
+print_r($arr);
+echo '<br/>sorted<br/>';
+$arr = insertionSort($arr, count($arr) - 1);
+print_r($arr);
