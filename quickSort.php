174727684ba375d169ca5a4385b3d3b6eac8b60a
unknown <pengjun@ycf.com>
2016-07-22 08:52:34 +0800

    quickSort.php

--- quickSort.php
+++ quickSort.php
@@ -1,13 +1,14 @@
 <?php
-function quickSort (array $arr,$left,$right) {
+function quickSort(array $arr, $left, $right)
+{
     $base = $left;
     $last = $right;
     while ($left < $right) {
         while ($arr[$base] <= $arr[$right] && $left < $right) {
-            $right --;
+            $right--;
         }
-        while ($arr[$base] >=  $arr[$left] && $left < $right) {
-            $left ++;
+        while ($arr[$base] >= $arr[$left] && $left < $right) {
+            $left++;
         }
         $temp = $arr [$right];
         $arr [$right] = $arr [$left];
@@ -17,16 +18,16 @@ function quickSort (array $arr,$left,$right) {
     $arr [$base] = $arr [$left];
     $arr [$left] = $temp;
     if ($left > $base) {
-        $arr = fastSort ($arr,$base,$left-1);
+        $arr = quickSort($arr, $base, $left - 1);
     }
     if ($last > $left) {
-        $arr = fastSort ($arr,$left+1,$last);
+        $arr = quickSort($arr, $left + 1, $last);
     }
     return $arr;
 }
 
-$arr = [12,45,7868,-87,45,754,154,5734,-57,2345,24,-5,73,26,2345];
+$arr = [12, 45, 7868, -87, 45, 754, 154, 5734, -57, 2345, 24, -5, 73, 26, 2345];
 print_r($arr);
 echo '<br/>';
-$arr = quickSort ($arr,0,count($arr)-1);
+$arr = quickSort($arr, 0, count($arr) - 1);
 print_r($arr);
