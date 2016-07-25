b8dc56fdf6e9034f454ace987a08cd7456748d69
unknown <pengjun@ycf.com>
2016-07-25 08:45:29 +0800

    加入注释

---
+++ InsertionSort.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * 使用插入排序的方法对数组进行从小到大排序
+ *
+ * @param array $arr 待排序数据的存储数组
+ * @param  int $end 待排序数列的末下标
+ * @param int $start 待排序数列的起始下标
+ * @return array 返回排序后的数组
+ */
+function insertionSort($arr, $end, $start = 0)
+{
+    $index = 1;
+    while ($end >= $index) {
+        $position = dichotomySearch($arr, $index - 1, $start, $arr[$index]);
+        $temp = $arr[$index];
+        for ($i = $index; $i > $position; $i--) {
+            $arr[$i] = $arr[$i - 1];
+        }
+        $arr[$position] = $temp;
+        $index++;
+    }
+    return $arr;
+}
+
+/**
+ * 使用二分法找出给出数据在数组中的下标值
+ *
+ * @param array $arr 数据存放的数组
+ * @param int $end 已排好序的数据末下标
+ * @param int $start 已排好数据的起始下标
+ * @param int $basic 待插入数据值
+ * @return int 返回待插入数据的插入位置
+ */
+function dichotomySearch($arr, $end, $start, $basic)
+{
+    if ($end - $start > 1) {
+        $middle = floor(($end + $start) / 2);
+        if ($arr[$middle] >= $basic) {
+            return dichotomySearch($arr, $middle, $start, $basic);
+        } else {
+            return dichotomySearch($arr, $end, $middle + 1, $basic);
+        }
+    } elseif ($basic < $arr[$start]) {
+        return $start;
+    } elseif ($basic < $arr[$end]) {
+        return $end;
+    } else {
+        return $end + 1;
+    }
+}
+
+$arr = [34, 6, 6, 75, 75, 254, 26, 885, 5463, -34, 6456, -435, -4, 525, 5643252];
+echo 'before:<br/>';
+print_r($arr);
+echo '<br/>sorted<br/>';
+$arr = insertionSort($arr, count($arr) - 1);
+print_r($arr);
--- MergeSort.php
+++ MergeSort.php
@@ -1,10 +1,18 @@
 <?php
-function mergeSort($array, $last, $first = 0)
+/**
+ * 用归并排序的方法对数据进行从小到大排序
+ *
+ * @param array $array 待排序数据的存放数组
+ * @param int $last 待排序数据在数组中的末下标
+ * @param int $first 待排序数据在数组中的起始下标
+ * @return array 返回排序后的数组
+ */
+function mergeSort(array $array, $last, $first = 0)
 {
     $middle = floor(($first + $last) / 2);
     if ($last - $first > 3) {
         $array = mergeSort($array, $last, $middle + 1);
-     //   print_r($array);
+        //   print_r($array);
         $array = mergeSort($array, $middle, $first);
     } else {
         $array = mSort($array, $last, $middle + 1);
@@ -16,32 +24,41 @@ function mergeSort($array, $last, $first = 0)
     while ($index1 <= $middle && $index2 <= $last) {
         if ($array[$index1] < $array[$index2]) {
             $temp[] = $array[$index1];
-            $index1 ++;
+            $index1++;
         } else {
             $temp[] = $array[$index2];
-            $index2 ++;
+            $index2++;
         }
     }
     if ($index1 > $middle) {
         while ($index2 <= $last) {
             $temp[] = $array[$index2];
-            $index2 ++;
+            $index2++;
         }
     } else {
         while ($index1 <= $middle) {
             $temp[] = $array[$index1];
-            $index1 ++;
+            $index1++;
         }
     }
-    for ($index1 = 0, $index2 = $first; $index1 < count($temp); $index1 ++, $index2 ++) {
-        $array[$index2] =$temp[$index1];
+    for ($index1 = 0, $index2 = $first; $index1 < count($temp); $index1++, $index2++) {
+        $array[$index2] = $temp[$index1];
     }
     return $array;
 }
 
-function mSort($array, $last, $first)
+/**
+ * 对指定范围内（一个或两个数）的数进行从小到大排序
+ *
+ * @param array $array 数据存放的数组
+ * @param int $last 指定数据范围的末下标
+ * @param int $first 指定数据范围的起始下标
+ * @return array 返回排序后的数组
+ */
+
+function mSort(array $array, $last, $first)
 {
-    if($last === $first) {
+    if ($last === $first) {
         return $array;
     } else {
         if ($array[$first] > $array[$last]) {
